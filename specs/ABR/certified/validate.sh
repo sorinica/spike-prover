5a9e3a5218dc9a43d1607779dbcfce365e58f1e8
sorinica <sorinica@ddda1572-dd43-0410-ae3b-c7f5af83fe6f>
2013-04-29 06:15:33 +0000

    creating utils.vo and coccinelle_utils.vo

--- specs/ABR/certified/validate.sh
+++ specs/ABR/certified/validate.sh
@@ -2,6 +2,17 @@
 echo "***************   Starting computation  **********************"
 
 j=0
+
+if [ ! -f utils.vo ] ; then
+    echo "... building utils.vo"
+    coqc utils.v
+fi
+
+if [ ! -f coccinelle_utils.vo ] ; then
+    echo "... building coccinelle_utils.vo"
+    coqc coccinelle_utils.v
+fi
+
 for i in firstat_timeat firstat_progat sorted_sorted sorted_e_two member_t_insin member_t_insat member_firstat timel_insat_t erl_insin erl_insat erl_prog time_progat_er timeat_tcrt null_listat null_listat1 cons_insat cons_listat timel_listupto time_listat null_wind2 sorted_cons_listat timel_insin1 null_listupto1 erl_cons no_time sorted_insin2 timel_timeat_max progat_timel_erl  progat_insat1 sorted_listupto sorted_insat1 final progat_insat
 
 do
