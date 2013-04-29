18450128b33b0cba880451484ad9874d7d0dd6ec
sorinica <sorinica@ddda1572-dd43-0410-ae3b-c7f5af83fe6f>
2013-04-29 06:23:00 +0000

    updated list of files

--- specs/basic/certified/validate.sh
+++ specs/basic/certified/validate.sh
@@ -13,7 +13,7 @@ if [ ! -f coccinelle_utils.vo ] ; then
     coqc coccinelle_utils.v
 fi
 
-for i in firstat_timeat firstat_progat sorted_sorted sorted_e_two member_t_insin member_t_insat member_firstat timel_insat_t erl_insin erl_insat erl_prog time_progat_er timeat_tcrt null_listat null_listat1 cons_insat cons_listat timel_listupto time_listat null_wind2 sorted_cons_listat timel_insin1 null_listupto1 erl_cons no_time sorted_insin2 timel_timeat_max progat_timel_erl  progat_insat1 sorted_listupto sorted_insat1 final progat_insat
+for i in sorted even_odd tree_list 
 
 do
 j=`expr $j + 1`
