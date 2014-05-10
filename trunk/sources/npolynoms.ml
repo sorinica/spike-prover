b4af5e5bfb975b691a3350b7aea97d75fb0b592c
sorinica <sorinica@ddda1572-dd43-0410-ae3b-c7f5af83fe6f>
2014-05-10 10:23:47 +0000

    small changes

--- trunk/sources/npolynoms.ml
+++ trunk/sources/npolynoms.ml
@@ -28,28 +28,24 @@ let measure t =
     match t#content with
       | Var_univ (i,_) ->   insert_sorted_dup (=) (fun x y -> y < x) i l
       | Var_exist _ -> l
-      | Term (f, l, _) -> List.fold_right (fun t' l' -> fn_univ t' l') l []
+      | Term (_, l1, _) -> List.fold_right (fun t' l' -> fn_univ t' l') l1 l
   in
   let rec fn_exist t l =
     match t#content with
       | Var_exist (i,_) ->   insert_sorted_dup (=) (fun x y -> y < x) i l
       | Var_univ _ -> l
-      | Term (f, l, _) -> List.fold_right (fun t' l' -> fn_exist t' l') l []
+      | Term (f, l1, _) -> List.fold_right (fun t' l' -> fn_exist t' l') l1 l
   in
   let sorted_vuniv = fn_univ t [] in
   let sorted_vexist = fn_exist t [] in
   (sorted_vuniv, sorted_vexist)
 
 let heavier t1 t2 = 
-  let (vu1, ve1) = measure t1 in
+   let (vu1, ve1) = measure t1 in
   let (vu2, ve2) = measure t2 in
   let rec fn_greater l1 l2 = 
     match l1 with
-      | [] -> 
-	(match l2 with 
-	  | [] -> false
-	  | h2 :: tl2 -> false
-	)
+      | [] -> false
       | h1 :: tl1 -> 
 	(match l2 with
 	  | [] -> true
@@ -155,12 +151,6 @@ let rec np_add t1 t2 i =
 		      new term (Term (id_symbol_plus, [fst_arg; t2'], id_sort_nat))
 		  else  new term (Term (id_symbol_plus, [t1; t2], id_sort_nat))
 	      )
-	    (* else  *)
-	    (*   if heavier t1 fst_arg then *)
-	    (* 	new term (Term (id_symbol_plus, [t1;t2], id_sort_nat)) *)
-	    (*   else  *)
-	    (* 	let snd_arg_norm = np_add t1 snd_arg (i+1) in *)
-	    (* 	new term (Term (id_symbol_plus, [fst_arg;snd_arg_norm], id_sort_nat)) *)
 	  else if f == id_symbol_times then
 	    let fst_arg = List.hd l in
 	    let snd_arg = List.hd (List.tl l) in
@@ -168,8 +158,8 @@ let rec np_add t1 t2 i =
 	      new term (Term (id_symbol_times, [(new term (Term (id_symbol_s,[fst_arg],id_sort_nat)));snd_arg], id_sort_nat))
 	    else if heavier t1 t2 then 
 	      new term (Term (id_symbol_plus, [t1;t2], id_sort_nat))
-	    else
-	      new term (Term (id_symbol_plus, [t2;t1], id_sort_nat))
+	    else	   
+	      new term (Term (id_symbol_plus, [t2; t1], id_sort_nat))
 	    else failwith "np_add"
       )
     | Term (f1, l1, _) -> 
