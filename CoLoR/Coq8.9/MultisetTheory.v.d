e7bcada6412e0daeb7ba9a296d65e49e760bab51
sorin stratulat <sorin@sorins-MacBook-Pro.local>
2019-01-28 10:04:09 +0100

    Coq 8.9

---
+++ CoLoR/Coq8.9/MultisetTheory.v.d
@@ -0,0 +1,2 @@
+MultisetTheory.vo MultisetTheory.glob MultisetTheory.v.beautified: MultisetTheory.v ./RelExtras.vo ./MultisetCore.vo ./ListPermutation.vo ./NatUtil.vo ./ListExtras.vo ./LogicUtil.vo
+MultisetTheory.vio: MultisetTheory.v ./RelExtras.vio ./MultisetCore.vio ./ListPermutation.vio ./NatUtil.vio ./ListExtras.vio ./LogicUtil.vio
---
+++ Coccinelle/Coq8.9/more_list.v.d
@@ -0,0 +1,2 @@
+more_list.vo more_list.glob more_list.v.beautified: more_list.v ./closure.vo
+more_list.vio: more_list.v ./closure.vio
