8ee366a1df19c80cb03073be8712b00f5b84d806
sorinica <sorinica@gmail.com>
2024-02-21 22:41:30 +0100

    Adding Coq8.19

---
+++ CoLoR/Coq8.19/.coqdeps.d
@@ -0,0 +1,32 @@
+AccUtil.vo AccUtil.glob AccUtil.v.beautified AccUtil.required_vo: AccUtil.v LogicUtil.vo RelUtil.vo
+AccUtil.vio: AccUtil.v LogicUtil.vio RelUtil.vio
+LogicUtil.vo LogicUtil.glob LogicUtil.v.beautified LogicUtil.required_vo: LogicUtil.v 
+LogicUtil.vio: LogicUtil.v 
+MultisetTheory.vo MultisetTheory.glob MultisetTheory.v.beautified MultisetTheory.required_vo: MultisetTheory.v RelExtras.vo MultisetCore.vo ListPermutation.vo NatUtil.vo ListExtras.vo LogicUtil.vo
+MultisetTheory.vio: MultisetTheory.v RelExtras.vio MultisetCore.vio ListPermutation.vio NatUtil.vio ListExtras.vio LogicUtil.vio
+BoolUtil.vo BoolUtil.glob BoolUtil.v.beautified BoolUtil.required_vo: BoolUtil.v LogicUtil.vo
+BoolUtil.vio: BoolUtil.v LogicUtil.vio
+MultisetCore.vo MultisetCore.glob MultisetCore.v.beautified MultisetCore.required_vo: MultisetCore.v RelExtras.vo
+MultisetCore.vio: MultisetCore.v RelExtras.vio
+NatUtil.vo NatUtil.glob NatUtil.v.beautified NatUtil.required_vo: NatUtil.v LogicUtil.vo EqUtil.vo BoolUtil.vo RelUtil.vo
+NatUtil.vio: NatUtil.v LogicUtil.vio EqUtil.vio BoolUtil.vio RelUtil.vio
+EqUtil.vo EqUtil.glob EqUtil.v.beautified EqUtil.required_vo: EqUtil.v LogicUtil.vo RelUtil.vo BoolUtil.vo
+EqUtil.vio: EqUtil.v LogicUtil.vio RelUtil.vio BoolUtil.vio
+MultisetList.vo MultisetList.glob MultisetList.v.beautified MultisetList.required_vo: MultisetList.v LogicUtil.vo RelExtras.vo MultisetCore.vo ListExtras.vo
+MultisetList.vio: MultisetList.v LogicUtil.vio RelExtras.vio MultisetCore.vio ListExtras.vio
+RelExtras.vo RelExtras.glob RelExtras.v.beautified RelExtras.required_vo: RelExtras.v LogicUtil.vo RelUtil.vo
+RelExtras.vio: RelExtras.v LogicUtil.vio RelUtil.vio
+ListExtras.vo ListExtras.glob ListExtras.v.beautified ListExtras.required_vo: ListExtras.v ListUtil.vo NatUtil.vo LogicUtil.vo
+ListExtras.vio: ListExtras.v ListUtil.vio NatUtil.vio LogicUtil.vio
+RelMidex.vo RelMidex.glob RelMidex.v.beautified RelMidex.required_vo: RelMidex.v LogicUtil.vo
+RelMidex.vio: RelMidex.v LogicUtil.vio
+ListPermutation.vo ListPermutation.glob ListPermutation.v.beautified ListPermutation.required_vo: ListPermutation.v ListExtras.vo NatUtil.vo LogicUtil.vo
+ListPermutation.vio: ListPermutation.v ListExtras.vio NatUtil.vio LogicUtil.vio
+MultisetNat.vo MultisetNat.glob MultisetNat.v.beautified MultisetNat.required_vo: MultisetNat.v MultisetTheory.vo MultisetOrder.vo MultisetList.vo RelExtras.vo
+MultisetNat.vio: MultisetNat.v MultisetTheory.vio MultisetOrder.vio MultisetList.vio RelExtras.vio
+RelUtil.vo RelUtil.glob RelUtil.v.beautified RelUtil.required_vo: RelUtil.v LogicUtil.vo RelMidex.vo
+RelUtil.vio: RelUtil.v LogicUtil.vio RelMidex.vio
+ListUtil.vo ListUtil.glob ListUtil.v.beautified ListUtil.required_vo: ListUtil.v LogicUtil.vo EqUtil.vo RelUtil.vo BoolUtil.vo NatUtil.vo
+ListUtil.vio: ListUtil.v LogicUtil.vio EqUtil.vio RelUtil.vio BoolUtil.vio NatUtil.vio
+MultisetOrder.vo MultisetOrder.glob MultisetOrder.v.beautified MultisetOrder.required_vo: MultisetOrder.v RelUtil.vo RelExtras.vo MultisetTheory.vo ListPermutation.vo MultisetCore.vo ListExtras.vo AccUtil.vo LogicUtil.vo
+MultisetOrder.vio: MultisetOrder.v RelUtil.vio RelExtras.vio MultisetTheory.vio ListPermutation.vio MultisetCore.vio ListExtras.vio AccUtil.vio LogicUtil.vio
