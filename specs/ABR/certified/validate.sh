960f27f9304cf65d32c840f0b132c8bdb9cf38d7
sorinica <sorinica@gmail.com>
2024-02-27 11:34:27 +0100

    change validate.sh

--- specs/ABR/certified/validate.sh
+++ specs/ABR/certified/validate.sh
@@ -3,30 +3,11 @@ echo "***************   Starting computation  **********************"
 
 j=0
 
-if [ ! -f utils.vo ] ; then
-    echo "... building utils.vo"
-    coqc utils.v
-fi
-
-if [ ! -f coccinelle_utils.vo ] ; then
-    echo "... building coccinelle_utils.vo"
-    coqc coccinelle_utils.v
-fi
-
 for i in firstat_timeat firstat_progat sorted_sorted sorted_e_two member_t_insin member_t_insat member_firstat timel_insat_t erl_insin erl_insat erl_prog time_progat_er timeat_tcrt null_listat null_listat1 cons_insat cons_listat timel_listupto time_listat null_wind2 sorted_cons_listat timel_insin1 null_listupto1 erl_cons no_time sorted_insin2 timel_timeat_max progat_timel_erl  progat_insat1 sorted_listupto sorted_insat1 final progat_insat
 
 do
 j=`expr $j + 1`
 echo "file $j: $i.spike"
-if [  "$1" == "-spec" ] ;then 
-    ../../../sources/spike_bc -coqc_spec ../no_rm/$i.spike > $i.proof 
-    echo "treating $i"'_spec.v' 
-    time coqc "$i"'_spec.v' 
-else 
-    ../../../sources/spike_bc -coqc ../no_rm/$i.spike > $i.proof
-    echo "treating $i.v" 
-    time coqc "$i.v" 
-fi
+    ../../../sources/_build/default/spike.exe -coqc_spec ../no_rm/$i.spike > $i.proof 
+    ../../../sources/_build/default/spike.exe -coqc ../no_rm/$i.spike > $i.proof
 done
-echo "***************   End computation  **********************"
-
--- specs/basic/certified/validate.sh
+++ specs/basic/certified/validate.sh
@@ -3,30 +3,13 @@ echo "***************   Starting computation  **********************"
 
 j=0
 
-if [ ! -f utils.vo ] ; then
-    echo "... building utils.vo"
-    coqc utils.v
-fi
-
-if [ ! -f coccinelle_utils.vo ] ; then
-    echo "... building coccinelle_utils.vo"
-    coqc coccinelle_utils.v
-fi
-
 for i in sorted even_odd tree_list 
 
 do
 j=`expr $j + 1`
 echo "file $j: $i.spike"
-if [  "$1" == "-spec" ] ;then 
-    ../../../sources/spike_bc -coqc_spec ../examples/$i.spike > $i.proof 
-    echo "treating $i"'_spec.v' 
-    time coqc "$i"'_spec.v' 
-else 
-    ../../../sources/spike_bc -coqc ../examples/$i.spike > $i.proof
-    echo "treating $i.v" 
-    time coqc "$i.v" 
-fi
+    ../../../sources/_build/default/spike.exe -coqc_spec ../examples/$i.spike > $i.proof 
+    ../../../sources/_build/default/spike.exe -coqc ../examples/$i.spike > $i.proof
 done
 echo "***************   End computation  **********************"
 
