06ae3b73297e2e6e38159d1d03eb09c9ea282af9
sorinica <sorinica@gmail.com>
2024-02-28 08:00:47 +0100

    delete temporary files

--- theories/Coccinelle/Coq8.13/dickson.v.d
+++
@@ -1,2 +0,0 @@
-dickson.vo dickson.glob dickson.v.beautified: dickson.v ./more_list.vo ./list_permut.vo ./ordered_set.vo ./closure.vo
-dickson.vio: dickson.v ./more_list.vio ./list_permut.vio ./ordered_set.vio ./closure.vio
--- theories/Coccinelle/Coq8.15/TransClosure.v.d
+++
@@ -1,2 +0,0 @@
-TransClosure.vo TransClosure.glob TransClosure.v.beautified: TransClosure.v
-TransClosure.vio: TransClosure.v
